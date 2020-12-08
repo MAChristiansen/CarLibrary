914d211376597443df5fc555f47fc9c62c64e27e
Marcus Christiansen <mac@trackman.com>
2020-12-08 13:56:12 +0100

    Public init

--- Sources/CarLibrary/Car.swift
+++ Sources/CarLibrary/Car.swift
@@ -5,6 +5,11 @@ public struct Car {
     public var color : String
     public var brand : Brand
     
+    public init(color : String, brand : Brand) {
+        self.color = color
+        self.brand = brand
+    }
+    
     public func start() {
         print("Brrrr - your super awesome car is started.")
     }
